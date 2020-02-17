e2d7522030fa8f87c1d8132aeb551d61209769f9
pighaddt <pighaddt@gmail.com>
2020-02-17 16:08:54 +0800

    14.抽取字串資源、如何讓APP擁有多國語言

--- app/src/main/java/com/angus/guesskotlin/MainActivity.kt
+++ app/src/main/java/com/angus/guesskotlin/MainActivity.kt
@@ -10,28 +10,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val secretNumber = SecretNumber()
+    val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "secret : " + secretNumber.secret)
+        Log.d(TAG, "secret : " + secretNumber.secret)
     }
 
     fun check(view : View){
         var n = ed_number.text.toString().toInt()
         println("number : $n")
-        Log.d("MainActivity", "number  $n")
+        Log.d(TAG, "number  $n")
         val diff = secretNumber.validate(n)
-        var message = "Yes, you got it"
+        var message = getString(R.string.yes_you_got_it)
         if (diff > 0){
-            message = "Smaller"
+            message = getString(R.string.smaller)
         }else if(diff < 0){
-            message = "Bigger"
+            message = getString(R.string.bigger)
         }
 //        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         AlertDialog.Builder(this)
-            .setTitle("Message")
+            .setTitle(getString(R.string.message))
             .setMessage(message)
-            .setPositiveButton("OK", null)
+            .setPositiveButton(getString(R.string.ok), null)
             .show()
         ed_number.setText("")
     }
